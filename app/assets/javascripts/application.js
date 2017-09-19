0a86b9f6806228682c62fc386feae7bfe8462369
Jay Deeh <jaad.mateo15@gmail.com>
2017-09-19 09:16:18 +0000

    layout and ui update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -56,26 +56,12 @@ function activateCheats() {
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "35%";
+    document.getElementById("main").style.marginLeft = "35%";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-}
-
-function openNav1() {
-    document.getElementById("mySidenav1").style.width = "100%";
-}
-
-function closeNav1() {
-    document.getElementById("mySidenav1").style.width = "0";
-}
-
-function openNav2() {
-    document.getElementById("mySidenav2").style.width = "15%";
-}
-
-function closeNav2() {
-    document.getElementById("mySidenav2").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
 }
 
 
